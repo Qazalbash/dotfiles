6f10c13c791dde12ace24d19c16d0b163c0cf9db
Meesum Qazalbash <meesumqazalbash@gmail.com>
2025-08-13 01:00:03 +0500

    chore: dotfiles on Aug 13, 2025 1:00 AM

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,21 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/gradf/miniforge3/bin/conda
-    eval /home/gradf/miniforge3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/gradf/miniforge3/etc/fish/conf.d/conda.fish"
-        . "/home/gradf/miniforge3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/gradf/miniforge3/bin" $PATH
-    end
-end
+set -x PATH $PATH /usr/local/cuda-12.9
+set -x PATH $PATH /usr/local/cuda-12.9/bin
+set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda-12.9/lib64
 
-if test -f "/home/gradf/miniforge3/etc/fish/conf.d/mamba.fish"
-    source "/home/gradf/miniforge3/etc/fish/conf.d/mamba.fish"
-end
-# <<< conda initialize <<<
-
-# neofetch --size 500px --chafa --crop_mode fit --source ~/cat.jpeg
+set -x PATH $PATH /usr/local/go/bin
