403fbf6e1888e4f721117a5c15a64872a6933f14
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-01-07 01:45:38 +0500

    chore: fancy lualine

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -1,16 +1,57 @@
 require('lualine').setup({
   options = {
-    icons_enabled = true,
-    theme = 'vscode',
-    component_separators = '|',
-    section_separators = '',
+    theme = 'auto',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    globalstatus = true,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 
+      { 'mode', icon = '' } -- Technical/CPU icon instead of Vim
+    },
+    lualine_b = {
+      { 'branch', icon = '󱓎' }, 
+      { 'diff', symbols = { added = '󰐕 ', modified = '󰏫 ', removed = '󰍴 ' } },
+    },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 3, 
+        symbols = { modified = '󰚠', readonly = '󰌾', unnamed = '' }
+      },
+    },
+    lualine_x = {
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        symbols = { error = '', warn = '!', info = ' ', hint = '󰌵 ' },
+      },
+      {
+        function()
+          local msg = 'No LSP'
+          local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+          local clients = vim.lsp.get_active_clients()
+          if next(clients) == nil then return msg end
+          for _, client in ipairs(clients) do
+            local filetypes = client.config.filetypes
+            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+              return client.name:upper()
+            end
+          end
+          return msg
+        end,
+        icon = '󰒋', 
+        color = { gui = 'bold' },
+        separator = { left = '' }, 
+      },
+    },
+    lualine_y = {
+      { 'encoding', fmt = string.upper },
+      { 'fileformat', icons_enabled = true },
+    },
+    lualine_z = {
+      { 'location', icon = '' },
+    },
   },
 })
