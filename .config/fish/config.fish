2cee5a245ae073be1c4a4b2f2ccc056bdd41a177
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-02-19 12:59:16 +0500

    updated dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# set -x PATH $PATH /usr/local/cuda-12.9
-# set -x PATH $PATH /usr/local/cuda-12.9/bin
-# set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda-12.9/lib64
+set -x PATH $PATH /usr/local/cuda-12.9
+set -x PATH $PATH /usr/local/cuda-12.9/bin
+set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda-12.9/lib64
 
 # set -x PATH $PATH /usr/local/go/bin
 
--- .config/nvim/lua/configs/lsp.lua
+++ .config/nvim/lua/configs/lsp.lua
@@ -1,4 +1,15 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "pyright" }
+    ensure_installed = {
+        "bashls",
+        "cspell_ls",
+        "gh_actions_ls",
+        "jsonls",
+        "lua_ls",
+        "pyright",
+        "ruff",
+        "rust_analyzer",
+        "ty",
+        "vimls"
+    }
 })
