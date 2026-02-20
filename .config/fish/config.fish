e96edc022a870432e029a70fb75c01f9d3ae4471
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-02-20 07:25:07 +0500

    chore: revise fish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,14 +2,16 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -x PATH $PATH /usr/local/cuda-12.9
-set -x PATH $PATH /usr/local/cuda-12.9/bin
-set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda-12.9/lib64
+set -g fish_greeting
 
-# set -x PATH $PATH /usr/local/go/bin
+# set -x PATH $PATH /usr/local/cuda-12.9
+# set -x PATH $PATH /usr/local/cuda-12.9/bin
+# set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /usr/local/cuda-12.9/lib64
 
+set -x PATH $PATH /usr/local/go/bin
 
 set -gx EDITOR "nvim" # For ranger file manager
 set -gx VISUAL "nvim" # For ranger file manager
 
 set -x PATH $PATH /opt/nvim-linux-x86_64/bin
+
